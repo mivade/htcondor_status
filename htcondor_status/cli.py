b49a62d92975652bf6751be0f39bd3f6c08ea6fc
Michael V. DePalatis <mike@depalatis.net>
2019-09-25 10:04:54 -0600

    Add static command to write files

--- htcondor_status/cli.py
+++ htcondor_status/cli.py
@@ -11,28 +11,6 @@ def make_serve_parser(subparsers: _SubParsersAction) -> ArgumentParser:
     return parser
 
 
-def make_json_parser(subparsers: _SubParsersAction) -> ArgumentParser:
-    """Make the ``json`` subcommand."""
-    parser = subparsers.add_parser("json")
-    parser.set_defaults(command=generate_json)
-    parser.add_argument("--indent", "-i", type=int, help="indent spaces")
-    parser.add_argument("--file", "-f", type=str, help="where to write the output")
-    return parser
-
-
-def main() -> None:
-    """Main CLI entry point."""
-    parser = ArgumentParser(prog="htcondor_status")
-    parser.set_defaults(command=lambda **_: parser.print_usage())
-    subparsers = parser.add_subparsers()
-    make_serve_parser(subparsers)
-    make_json_parser(subparsers)
-    namespace = parser.parse_args()
-    args = vars(namespace)
-    command = args.pop("command")
-    command(**args)
-
-
 def serve(*, port: int, debug: bool) -> None:
     """Run the server.
 
@@ -43,6 +21,15 @@ def serve(*, port: int, debug: bool) -> None:
     from htcondor_status.server import main
 
     main(port, debug)
+
+
+def make_json_parser(subparsers: _SubParsersAction) -> ArgumentParser:
+    """Make the ``json`` subcommand."""
+    parser = subparsers.add_parser("json")
+    parser.set_defaults(command=generate_json)
+    parser.add_argument("--indent", "-i", type=int, help="indent spaces")
+    parser.add_argument("--file", "-f", type=str, help="where to write the output")
+    return parser
 
 
 def generate_json(*, file: Optional[str], indent: Optional[int]) -> None:
@@ -63,6 +50,43 @@ def generate_json(*, file: Optional[str], indent: Optional[int]) -> None:
     else:
         with open(file, "w") as f:
             json.dump(data, f, indent=indent)
+
+
+def make_static_parser(subparsers: _SubParsersAction) -> ArgumentParser:
+    """Make the ``static`` subcommand to write all static files."""
+    parser = subparsers.add_parser("static")
+    parser.set_defaults(command=write_static_files)
+    parser.add_argument("directory", type=str, help="directory to write files to")
+    return parser
+
+
+def write_static_files(*, directory: str) -> None:
+    """Write the static files to the given directory."""
+    from pathlib import Path
+    import pkgutil
+
+    path = Path(directory)
+    path.mkdir(parents=True, exist_ok=True)
+
+    for resource in ("condor.jpg", "favicon.ico", "index.html"):
+        data = pkgutil.get_data("htcondor_status.static", resource)
+        filepath = path.joinpath(resource)
+        print(f"Writing {filepath}")
+        filepath.write_bytes(data)
+
+
+def main() -> None:
+    """Main CLI entry point."""
+    parser = ArgumentParser(prog="htcondor_status")
+    parser.set_defaults(command=lambda **_: parser.print_usage())
+    subparsers = parser.add_subparsers()
+    make_serve_parser(subparsers)
+    make_json_parser(subparsers)
+    make_static_parser(subparsers)
+    namespace = parser.parse_args()
+    args = vars(namespace)
+    command = args.pop("command")
+    command(**args)
 
 
 if __name__ == "__main__":
