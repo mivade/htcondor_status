0aec9e9462da3508f232488dd98f34a6ac86cb0f
Michael V. DePalatis <mike@depalatis.net>
2019-12-16 15:38:26 -0700

    Optionally add -global to condor_q command
    Make this the default. Closes #7.

--- htcondor_status/jobs.py
+++ htcondor_status/jobs.py
@@ -4,16 +4,22 @@ from subprocess import PIPE
 from typing import Any, Dict, List
 
 
-async def get_jobs(all: bool = True) -> List[Dict[str, Any]]:
+async def get_jobs(all: bool = True, global_: bool = True) -> List[Dict[str, Any]]:
     args = ["condor_q", "-json"]
+
+    if global_:
+        args.append("-global")
 
     if all:
         args.append("-all")
 
     p = await asyncio.create_subprocess_exec(*args, stdout=PIPE)
     out, _ = await p.communicate()
-    out = out or "[]"
-    return json.loads(out)
+
+    try:
+        return json.loads(out)
+    except json.JSONDecodeError:
+        return []
 
 
 if __name__ == "__main__":
