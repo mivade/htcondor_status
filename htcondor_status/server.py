6c36f0f89d704284ef393091e1dbc3f9bfaaac3b
Michael V. DePalatis <mike@depalatis.net>
2022-07-11 14:52:33 -0600

    Send GlobalJobId with /summary.json

--- htcondor_status/server.py
+++ htcondor_status/server.py
@@ -49,6 +49,7 @@ class JobSummaryHandler(RequestHandler):
             {
                 "jobs": jobs[
                     [
+                        "GlobalJobId",
                         "ClusterId",
                         "QDate",
                         "Owner",
